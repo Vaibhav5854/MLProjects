f29cbfcce3503867ada2bec045f6e8b0ddb1ea4d
Vaibhav5854 <vaibhav.src7@gmail.com>
2026-06-07 00:57:08 +0530

    Final:)

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from src.exception import CustomException
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
@@ -35,6 +36,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
     
-    except Exception as e :
-        raise CustomException(e,sys)
+
